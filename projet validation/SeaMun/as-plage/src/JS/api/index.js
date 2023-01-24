1ceb614903dbee5922c239c7ab4eace16088d2f0
oumaymamachraoui <105208718+oumaymamachraoui@users.noreply.github.com>
2023-01-24 16:47:55 +0100

    Delete projet validation/SeaMun directory

--- projet validation/SeaMun/as-plage/src/JS/api/index.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const fetchDocs = () => axios.get("/api/document/get");
-export const createDoc= (newDoc) => axios.post("/api/document/add", newDoc);
-export const updateDoc = (id, updatedDoc) => axios.patch(`/api/document/edit/${id}`, updatedDoc);
-export const deleteDoc = (id) => axios.delete(`/api/document/delete/${id}`);
--- projet validation/SeaMun/models/Documents.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const schema = mongoose.Schema;
-
-const documentSchema = new schema({
-  description: String,
-});
-
-module.exports = Documents = mongoose.model("document", documentSchema);
